151c05cf0bbeb596dce1b2a95211dacf6f0b09f3
sandz <407221377@qq.com>
2019-07-03 18:09:02 +0800

    ceshi

---
+++ .huskyrc.js
@@ -0,0 +1,10 @@
+const tasks = arr => arr.join(" && ");
+//查看支持的钩子函数可以
+//https://git-scm.com/docs/githooks
+
+module.exports = {
+  hooks: {
+    "pre-commit": tasks(["npx git-cz"])
+    /* "pre-push": "" */
+  }
+};
